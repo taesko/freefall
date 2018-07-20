e6b4e560c1b58cbd973ff5c47f20b1314ab1e1c3
Antonio Todorov <taeskow@gmail.com>
2018-07-20 11:44:43 +0300

    Add init-database.sql script

---
+++ scripts/init-database.sql
@@ -0,0 +1,5 @@
+.read scripts/create-database.sql
+.read scripts/insert-airports.sql
+.read scripts/insert-subscriptions.sql
+.read scripts/insert-users.sql
+.read scripts/insert-user-subscriptions.sql
