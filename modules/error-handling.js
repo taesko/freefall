b44edebac4ef557854d3d03bd88471c9efe46d2c
Antonio Todorov <taeskow@gmail.com>
2018-07-24 17:11:56 +0300

    Add error codes.

--- modules/error-handling.js
+++ modules/error-handling.js
@@ -3,6 +3,24 @@ class PeerError extends CustomError {}
 class AppError extends CustomError {}
 class UserError extends CustomError {}
 
+/*
+  2000-3000 - Peer errors
+    2000-2100 - Protocol errors
+    2100-2200 - Subscription errors
+    2200-2300 - Permission, authentication and registration of users errors
+    2900-3000 - Uncatalogued errors
+  3000-4000 - User errors
+  4000-5000 - App errors
+*/
+const errorCodes = {
+  subscriptionExists: '2100',
+  subscriptionDoesNotExist: '2101',
+  userDoesNotExist: '2200',
+  userAlreadyExists: '2201',
+  notEnoughPermissions: '2210',
+  unknownAirport: '2900',
+};
+
 function assertApp (assert, errMsg) {
   if (!assert) {
     throw new AppError(errMsg);
@@ -28,4 +46,5 @@ module.exports = {
   PeerError,
   AppError,
   UserError,
+  errorCodes,
 };
