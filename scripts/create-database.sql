5be9ac8cb42ec0c03162e001c20175611c2230bc
Hristo Spasov <hristo_sp99@abv.bg>
2018-07-28 01:22:06 +0300

    Indentation fix

--- scripts/create-database.sql
+++ scripts/create-database.sql
@@ -110,9 +110,9 @@ CREATE TABLE routes_flights (
 );
 
 CREATE TABLE account_transfers (
-	id serial PRIMARY KEY NOT NULL,
+  id serial PRIMARY KEY NOT NULL,
   user_id integer NOT NULL,
-	transfer_amount integer NOT NULL,
+  transfer_amount integer NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
