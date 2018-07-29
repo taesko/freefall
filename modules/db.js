42d121d2c9b657cab58f761dc1105d2afc481abd
Antonio Todorov <taeskow@gmail.com>
2018-07-29 14:32:17 +0300

    Remove nesting from db module.

--- modules/db.js
+++ modules/db.js
@@ -1,116 +1,121 @@
-module.exports = (() => {
-  const path = require('path');
-  const sqlite = require('sqlite');
-  const { assertApp, assertPeer } = require('./error-handling');
-  const { isObject } = require('lodash');
-  const log = require('./log');
-  let db;
-  let dbInitialized = false;
+const path = require('path');
+const sqlite = require('sqlite');
+const { assertApp, assertPeer } = require('./error-handling');
+const { isObject } = require('lodash');
+const log = require('./log');
+let db;
+let dbInitialized = false;
 
-  async function dbConnect () {
-    if (
-      dbInitialized
-    ) {
-      log.info('Already connected to freefall.db...');
-    } else {
-      log.info('Connecting to freefall.db...');
-      dbInitialized = true;
-      db = await sqlite.open(path.join(__dirname, '../freefall.db'));
-      await db.run('PRAGMA foreign_keys = ON;');
-      await db.run('PRAGMA integrity_check;');
-      log.info('freefall.db OK');
-    }
+async function dbConnect () {
+  if (
+    dbInitialized
+  ) {
+    log.info('Already connected to freefall.db...');
+  } else {
+    log.info('Connecting to freefall.db...');
+    dbInitialized = true;
+    db = await sqlite.open(path.join(__dirname, '../freefall.db'));
+    await db.run('PRAGMA foreign_keys = ON;');
+    await db.run('PRAGMA integrity_check;');
+    log.info('freefall.db OK');
+  }
+}
+
+function assertDB () {
+  assertApp(
+    isObject(db) &&
+    isObject(db.driver) &&
+    db.driver.open,
+    'No database connection.',
+  );
+}
+
+function stringifyColumns (columns) {
+  if (columns == null || columns === '*') {
+    return '*';
+  } else {
+    return columns.join(', ');
+  }
+}
+
+function buildWhereClause (where) {
+  if (where == null) {
+    return {
+      whereClause: `WHERE 1`,
+      valueHash: {},
+    };
   }
 
-  function assertDB () {
-    assertApp(
-      isObject(db) &&
-      isObject(db.driver) &&
-      db.driver.open,
-      'No database connection.',
-    );
-  }
+  assertApp(isObject(where), 'buildWhereClause\'s parameter must be a hash.');
 
-  function stringifyColumns (columns) {
-    if (columns == null || columns === '*') {
-      return '*';
-    } else {
-      return columns.join(', ');
-    }
-  }
-
-  function buildWhereClause (where) {
-    if (where == null) {
-      return { whereClause: `WHERE 1`, valueHash: {} };
-    }
-
-    assertApp(isObject(where), 'buildWhereClause\'s parameter must be a hash.');
-
-    const whereEntries = Object.entries(where);
-    const whereColumns = whereEntries.map(([column]) => `${column}=$where_value_${column}`)
-      .join(' AND ');
-    const valueHash = whereEntries
-      .map(([column, value]) => [`$where_value_${column}`, value])
-      .reduce(
-        (hash, entries) => {
-          hash[entries[0]] = entries[1];
-          return hash;
-        },
-        {},
-      );
-
-    return { whereClause: `WHERE ${whereColumns}`, valueHash };
-  }
-
-  async function executeAll (...args) {
-    assertDB();
-    return db.all(...args);
-  }
-
-  async function executeRun (...args) {
-    assertDB();
-    return db.run(...args);
-  }
-
-  async function select (table, columns = '*') {
-    assertDB();
-    assertApp(
-      typeof table === 'string',
-      'Expected string for a name of table',
+  const whereEntries = Object.entries(where);
+  const whereColumns = whereEntries.map(([column]) => `${column}=$where_value_${column}`)
+    .join(' AND ');
+  const valueHash = whereEntries
+    .map(([column, value]) => [`$where_value_${column}`, value])
+    .reduce(
+      (hash, entries) => {
+        hash[entries[0]] = entries[1];
+        return hash;
+      },
+      {},
     );
 
-    return db.all(`SELECT ${stringifyColumns(columns)} FROM ${table};`);
-  }
+  return {
+    whereClause: `WHERE ${whereColumns}`,
+    valueHash,
+  };
+}
 
-  async function selectWhere (table, columns, where) {
-    assertDB();
-    assertApp(
-      typeof table === 'string' &&
-      isObject(where) &&
-      'Invalid select data',
-    );
+async function executeAll (...args) {
+  assertDB();
+  return db.all(...args);
+}
 
-    const { whereClause, valueHash } = buildWhereClause(where);
-    const query = `SELECT ${stringifyColumns(columns)} FROM ${table} ${whereClause}`;
+async function executeRun (...args) {
+  assertDB();
+  return db.run(...args);
+}
 
-    log.debug('Executing query', query, 'replaced with hash:', valueHash);
+async function select (table, columns = '*') {
+  assertDB();
+  assertApp(
+    typeof table === 'string',
+    'Expected string for a name of table',
+  );
 
-    return db.all(
-      query,
-      valueHash,
-    );
-  }
+  return db.all(`SELECT ${stringifyColumns(columns)} FROM ${table};`);
+}
 
-  async function selectRoutesFlights (fetchId, params) {
-    assertDB();
-    assertApp(
-      Number.isInteger(fetchId),
-      'Invalid fetch id.',
-    );
+async function selectWhere (table, columns, where) {
+  assertDB();
+  assertApp(
+    typeof table === 'string' &&
+    isObject(where) &&
+    'Invalid select data',
+  );
 
-    const queryParams = [fetchId];
+  const { whereClause, valueHash } = buildWhereClause(where);
+  const query = `SELECT ${stringifyColumns(columns)} FROM ${table} ${whereClause}`;
 
-    let query = `
+  log.debug('Executing query', query, 'replaced with hash:', valueHash);
+
+  return db.all(
+    query,
+    valueHash,
+  );
+}
+
+async function selectRoutesFlights (fetchId, params) {
+  assertDB();
+  assertApp(
+    Number.isInteger(fetchId),
+    'Invalid fetch id.',
+  );
+
+  const queryParams = [fetchId];
+
+  let query = `
 
       SELECT
       routes.id AS routeId,
@@ -136,47 +141,47 @@ module.exports = (() => {
 
     `;
 
-    if (params.price_to) {
-      assertPeer(Number.isInteger(params.price_to), 'Invalid price in search request.');
-      query += ' AND routes.price <= ? ';
-      queryParams.push(params.price_to);
-    }
-
-    // TODO finish filtration
-
-    if (params.date_from) {
-      assertPeer(typeof params.date_from === 'string', 'Invalid date_from in search request.');
-      query += ' AND flights.dtime >= ?';
-      queryParams.push(params.date_from);
-    }
-
-    if (params.date_to) {
-      assertPeer(typeof params.date_to === 'string', 'Invalid date_to in search request.');
-      query += ' AND flights.atime <= ?';
-      queryParams.push(params.date_to);
-    }
-
-    query += ';';
-
-    const routesFlights = await db.all(query, queryParams);
-
-    assertApp(
-      Array.isArray(routesFlights),
-      'Invalid db routes and flights response.',
-    );
-
-    return routesFlights;
+  if (params.price_to) {
+    assertPeer(Number.isInteger(params.price_to), 'Invalid price in search request.');
+    query += ' AND routes.price <= ? ';
+    queryParams.push(params.price_to);
   }
 
-  async function selectSubscriptions (airportFromId, airportToId) {
-    assertDB();
-    assertApp(
-      Number.isInteger(airportFromId) &&
-      Number.isInteger(airportToId),
-      'Invalid airport ids.',
-    );
+  // TODO finish filtration
 
-    const subscriptions = await db.all(`
+  if (params.date_from) {
+    assertPeer(typeof params.date_from === 'string', 'Invalid date_from in search request.');
+    query += ' AND flights.dtime >= ?';
+    queryParams.push(params.date_from);
+  }
+
+  if (params.date_to) {
+    assertPeer(typeof params.date_to === 'string', 'Invalid date_to in search request.');
+    query += ' AND flights.atime <= ?';
+    queryParams.push(params.date_to);
+  }
+
+  query += ';';
+
+  const routesFlights = await db.all(query, queryParams);
+
+  assertApp(
+    Array.isArray(routesFlights),
+    'Invalid db routes and flights response.',
+  );
+
+  return routesFlights;
+}
+
+async function selectSubscriptions (airportFromId, airportToId) {
+  assertDB();
+  assertApp(
+    Number.isInteger(airportFromId) &&
+    Number.isInteger(airportToId),
+    'Invalid airport ids.',
+  );
+
+  const subscriptions = await db.all(`
 
       SELECT fetches.id AS fetchId, fetches.timestamp FROM fetches
       LEFT JOIN subscriptions ON fetches.subscription_id = subscriptions.id
@@ -186,149 +191,149 @@ module.exports = (() => {
 
     `, [airportFromId, airportToId]);
 
-    assertApp(
-      Array.isArray(subscriptions),
-      'Invalid db select subscription response.',
-    );
+  assertApp(
+    Array.isArray(subscriptions),
+    'Invalid db select subscription response.',
+  );
 
-    return subscriptions;
+  return subscriptions;
+}
+
+async function insert (table, data) {
+  assertDB();
+  assertApp(
+    typeof table === 'string' &&
+    isObject(data),
+    'Invalid insert data.',
+  );
+
+  const columns = [];
+  const values = [];
+
+  for (const [col, value] of Object.entries(data)) {
+    columns.push(col);
+    values.push(value);
   }
 
-  async function insert (table, data) {
-    assertDB();
-    assertApp(
-      typeof table === 'string' &&
-      isObject(data),
-      'Invalid insert data.',
-    );
+  const columnsStringified = columns.join(', ');
+  const rowStringified = Array(values.length).fill('?').join(', ');
+  const insertResult = await db.run(`INSERT INTO ${table} (${columnsStringified}) VALUES (${rowStringified});`,
+    values,
+  );
 
-    const columns = [];
-    const values = [];
+  assertApp(
+    isObject(insertResult) &&
+    isObject(insertResult.stmt) &&
+    Number.isInteger(insertResult.stmt.lastID),
+    'Incorrect db response.',
+  );
 
-    for (const [col, value] of Object.entries(data)) {
-      columns.push(col);
-      values.push(value);
-    }
+  return insertResult.stmt.lastID;
+}
 
-    const columnsStringified = columns.join(', ');
-    const rowStringified = Array(values.length).fill('?').join(', ');
-    const insertResult = await db.run(`INSERT INTO ${table} (${columnsStringified}) VALUES (${rowStringified});`,
-      values,
-    );
+async function insertDataFetch (subscriptionId) {
+  assertDB();
 
-    assertApp(
-      isObject(insertResult) &&
-      isObject(insertResult.stmt) &&
-      Number.isInteger(insertResult.stmt.lastID),
-      'Incorrect db response.',
-    );
+  const newFetchResult = await db.run(
+    'INSERT INTO fetches(timestamp, subscription_id) VALUES (strftime(\'%Y-%m-%dT%H:%M:%SZ\' ,\'now\'), ?);',
+    [subscriptionId],
+  );
 
-    return insertResult.stmt.lastID;
+  assertApp(
+    isObject(newFetchResult) &&
+    isObject(newFetchResult.stmt) &&
+    Number.isInteger(newFetchResult.stmt.lastID),
+    'Incorrect db response.',
+  );
+
+  return newFetchResult.stmt.lastID;
+}
+
+async function insertIfNotExists (table, data, existsCheck) {
+  assertDB();
+
+  const found = await selectWhere(table, Object.keys(data), existsCheck);
+
+  assertApp(Array.isArray(found), 'Invalid db response.');
+
+  if (found.length > 0) {
+    assertApp(found.length === 1, 'Invalid db response.');
+
+    // return flightIdResult[0].id;
+    return false;
   }
 
-  async function insertDataFetch (subscriptionId) {
-    assertDB();
+  const insertResult = await insert(table, data);
 
-    const newFetchResult = await db.run(
-      'INSERT INTO fetches(timestamp, subscription_id) VALUES (strftime(\'%Y-%m-%dT%H:%M:%SZ\' ,\'now\'), ?);',
-      [subscriptionId],
+  assertApp(
+    Number.isInteger(insertResult),
+    'Incorrect db response.',
+  );
+
+  return true;
+}
+
+async function update (table, setHash, whereHash) {
+  assertApp(
+    isObject(setHash),
+    `update function requires setHash to be an object. setHash=${setHash}`,
+  );
+
+  const {
+    whereClause,
+    valueHash: whereValueHash,
+  } = buildWhereClause(whereHash);
+  const updateColumns = Object.entries(setHash)
+    .map(([column]) => `${column}=$update_value_${column}`)
+    .join(', ');
+  const updateValueHash = Object.entries(setHash)
+    .map(([column, value]) => [`$update_value_${column}`, value])
+    .reduce(
+      (hash, [column, value]) => {
+        hash[column] = value;
+        return hash;
+      },
+      {},
     );
+  const valueHash = Object.assign(updateValueHash, whereValueHash);
+  const query = `UPDATE ${table} SET ${updateColumns} ${whereClause}`;
+  log.debug('Executing query: ', query, 'replace with hash', valueHash);
 
-    assertApp(
-      isObject(newFetchResult) &&
-      isObject(newFetchResult.stmt) &&
-      Number.isInteger(newFetchResult.stmt.lastID),
-      'Incorrect db response.',
-    );
+  return executeRun(query, valueHash);
+}
 
-    return newFetchResult.stmt.lastID;
-  }
+async function updateWhere (table, setHash, whereHash) {
+  assertApp(
+    isObject(setHash) && isObject(whereHash),
+    `updateWhere function requires setHash and whereHash parameters to be objects. setHash=${setHash} whereHash=${whereHash}`,
+  );
 
-  async function insertIfNotExists (table, data, existsCheck) {
-    assertDB();
+  return update(table, setHash, whereHash);
+}
 
-    const found = await selectWhere(table, Object.keys(data), existsCheck);
+// TODO move out with selectSubs...
+async function updateEmailSub (email) {
+  // TODO bad name rename if you can
+  let rows;
 
-    assertApp(Array.isArray(found), 'Invalid db response.');
-
-    if (found.length > 0) {
-      assertApp(found.length === 1, 'Invalid db response.');
-
-      // return flightIdResult[0].id;
-      return false;
-    }
-
-    const insertResult = await insert(table, data);
-
-    assertApp(
-      Number.isInteger(insertResult),
-      'Incorrect db response.',
-    );
-
-    return true;
-  }
-
-  async function update (table, setHash, whereHash) {
-    assertApp(
-      isObject(setHash),
-      `update function requires setHash to be an object. setHash=${setHash}`,
-    );
-
-    const {
-      whereClause,
-      valueHash: whereValueHash,
-    } = buildWhereClause(whereHash);
-    const updateColumns = Object.entries(setHash)
-      .map(([column]) => `${column}=$update_value_${column}`)
-      .join(', ');
-    const updateValueHash = Object.entries(setHash)
-      .map(([column, value]) => [`$update_value_${column}`, value])
-      .reduce(
-        (hash, [column, value]) => {
-          hash[column] = value;
-          return hash;
-        },
-        {},
-      );
-    const valueHash = Object.assign(updateValueHash, whereValueHash);
-    const query = `UPDATE ${table} SET ${updateColumns} ${whereClause}`;
-    log.debug('Executing query: ', query, 'replace with hash', valueHash);
-
-    return executeRun(query, valueHash);
-  }
-
-  async function updateWhere (table, setHash, whereHash) {
-    assertApp(
-      isObject(setHash) && isObject(whereHash),
-      `updateWhere function requires setHash and whereHash parameters to be objects. setHash=${setHash} whereHash=${whereHash}`,
-    );
-
-    return update(table, setHash, whereHash);
-  }
-
-  // TODO move out with selectSubs...
-  async function updateEmailSub (email) {
-    // TODO bad name rename if you can
-    let rows;
-
-    try {
-      rows = await db.all(`
+  try {
+    rows = await db.all(`
       SELECT id, MAX(DateTime(timestamp))
       FROM fetches
     `);
-    } catch (e) {
-      assertApp(false, `Couldn't fetch most recent fetch id. Reason: ${e}`);
-    }
+  } catch (e) {
+    assertApp(false, `Couldn't fetch most recent fetch id. Reason: ${e}`);
+  }
 
-    assertApp(
-      rows.length === 1,
-      'There are different recent fetches with the same timestamp',
-    );
+  assertApp(
+    rows.length === 1,
+    'There are different recent fetches with the same timestamp',
+  );
 
-    const fetchId = rows[0].id;
+  const fetchId = rows[0].id;
 
-    const query = await db.prepare(
-      `
+  const query = await db.prepare(
+    `
       UPDATE user_subscriptions
       SET fetch_id_of_last_send=?
       WHERE user_id IN (
@@ -337,28 +342,27 @@ module.exports = (() => {
         WHERE email=?
       )
       `,
-      [fetchId, email],
-    );
+    [fetchId, email],
+  );
 
-    log.info('Updating subscription on email', email, 'to most recent fetch_id', fetchId);
-    log.debug('Executing query', query);
+  log.info('Updating subscription on email', email, 'to most recent fetch_id', fetchId);
+  log.debug('Executing query', query);
 
-    return query.all();
-  }
+  return query.all();
+}
 
-  return {
-    executeAll,
-    executeRun,
-    select,
-    insert,
-    insertDataFetch,
-    insertIfNotExists,
-    selectSubscriptions,
-    selectRoutesFlights,
-    selectWhere,
-    update,
-    updateWhere,
-    updateEmailSub,
-    dbConnect,
-  };
-})();
+module.exports = {
+  executeAll,
+  executeRun,
+  select,
+  insert,
+  insertDataFetch,
+  insertIfNotExists,
+  selectSubscriptions,
+  selectRoutesFlights,
+  selectWhere,
+  update,
+  updateWhere,
+  updateEmailSub,
+  dbConnect,
+};
