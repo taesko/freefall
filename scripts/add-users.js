607d3005edde518dd7858e590871c8ba318b7b30
Antonio Todorov <taeskow@gmail.com>
2018-07-31 13:25:00 +0300

    Add additional script add-users.js to populate db with customer users.

---
+++ scripts/add-users.js
@@ -0,0 +1,24 @@
+const log = require('../modules/log');
+const users = require('../modules/users');
+const accounting = require('../modules/accounting');
+
+const TEST_CREDITS = 2000;
+const demoUsers = [
+  {email: 'antonio@freefall.org', password: 'onetosix'},
+  {email: 'hristo@freefall.org', password: 'onetosix'},
+];
+
+async function main () {
+  for (const credentials of demoUsers) {
+    const { email } = credentials;
+    const password = users.hashPassword(credentials.password);
+    const user = await users.addUser({ email, password, role: 'customer' });
+    await accounting.depositCredits(user.id, TEST_CREDITS);
+  }
+}
+
+main()
+  .then(result => log.info('Inserted users: ', demoUsers))
+  .catch(reason => {
+    log.critical('Script failed due to error - ', reason);
+});
