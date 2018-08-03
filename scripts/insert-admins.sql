0fb17ad746bfa148c7ccff53c15a2bc3abf6921b
Antonio Todorov <taeskow@gmail.com>
2018-08-03 15:27:06 +0300

    Add constraints to length of emails and passwords.
    Also changes admin password.

--- scripts/insert-admins.sql
+++ scripts/insert-admins.sql
@@ -1,4 +1,4 @@
 INSERT INTO users
     (email, password, api_key, role)
 VALUES
-    ('admin@freefall.org', '81dc9bdb52d04dc20036dbd8313ed055', 'admin_api_key_magical_uncrackable_string', 'admin'); -- password is hashed
+    ('admin@freefall.org', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin_api_key_magical_uncrackable_string', 'admin'); -- password is hashed
