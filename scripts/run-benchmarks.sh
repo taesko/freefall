c6035c9361a7dd11cc64cca7c1d0868566ff9344
Antonio Todorov <taeskow@gmail.com>
2018-08-10 16:40:40 +0300

    Modify run-benchmarks to run benchmarks on admin portal as well.

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -13,20 +13,28 @@ fi
 
 if [ -z "$2" ]; then
     echo "Second argument not specified for url to benchmark"
-    url="http://10.20.1.110:3000/"
-    echo "Defaulting to ${url}"
+    url="http://10.20.1.128:3000/"
+    admin_url="http://10.20.1.128:3001"
+    echo "Defaulting to ${url} on port 3000 (server) and 3001 (admin)"
 else
-    url="http://${2}/"
+    url="http://${2}:3000"
+    admin_url="http://${2}:3001"
 fi
 
 number_of_requests=2000
 
 function run_post_benchmark {
+    if [ "$2" = "admin" ]; then
+        target_url="${admin_url}/api"
+    else
+        target_url=${url}
+    fi
+
     for concurrency in 100 500 1000; do
         output_file="${output_dir}/${1}-${concurrency}.stdout"
-        echo "Benchmarking $1 with ${concurrency} concurrency."
+        echo "Benchmarking $1 with ${concurrency} concurrency on url ${target_url}."
         echo "Output file is ${output_file}"
-        ab -n ${number_of_requests} -c ${concurrency} -T 'application/json' -p "./scripts/ab-tests/request-bodies/${1}-body.json" ${url} > ${output_file}
+        ab -n ${number_of_requests} -c ${concurrency} -T 'application/json' -p "./scripts/ab-tests/request-bodies/${1}_body.json" ${target_url} > ${output_file}
     done
 }
 
@@ -42,4 +50,7 @@ function run_get_benchmark {
 run_post_benchmark "search"
 run_post_benchmark "subscribe"
 run_post_benchmark "unsubscribe"
+run_post_benchmark "edit_subscription"
+run_post_benchmark "admin_list_airports" admin
+run_post_benchmark "admin_list_subscriptions" admin
 run_get_benchmark "get-homepage"
