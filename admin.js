fbee6d95822119a3248ce442166464717b2a9d28
Hristo Spasov <hristo_sp99@abv.bg>
2018-08-17 23:25:12 +0300

    Add pagination for account transfers page

--- admin.js
+++ admin.js
@@ -16,7 +16,6 @@ const { getAdminContext } = require('./modules/render-contexts');
 const { rpcAPILayer } = require('./modules/api');
 const {
   assertApp,
-  assertUser,
 } = require('./modules/error-handling.js');
 
 const app = new Koa();
@@ -25,7 +24,7 @@ const SESSION_CONFIG = {
   key: 'koa:sess:admin',
   maxAge: 1000 * 60 * 60 * 24, // 24 hours in miliseconds
 };
-const RESULTS_LIMIT = 5;
+const RESULTS_LIMIT = 20;
 
 app.keys = ['freefall is love freefall is life'];
 
@@ -169,8 +168,11 @@ router.get('/fetches', auth.redirectWhenLoggedOut('/login'), async (ctx) => {
     page = Number(ctx.query.page);
   }
 
-  assertUser(Number.isInteger(page), 'Expected integer for page number!');
-  assertUser(page >= 1, 'Expected positive integer for page number!');
+  if (!Number.isInteger(page) || page < 1) {
+    ctx.body = 'Expected positive integer for page number!';
+    ctx.status = 400;
+    return;
+  }
 
   const offset = (page - 1) * RESULTS_LIMIT;
   const fetchesResponse = await dbClient.executeQuery(`
@@ -191,8 +193,6 @@ router.get('/fetches', auth.redirectWhenLoggedOut('/login'), async (ctx) => {
 
   assertApp(isObject(fetchesResponse), `got ${fetchesResponse}`);
   assertApp(Array.isArray(fetchesResponse.rows), `got ${fetchesResponse.rows}`);
-
-  let totalAPIRequests = 0;
 
   const totalFetchesResponse = await dbClient.executeQuery(`
 
@@ -222,10 +222,80 @@ router.get('/fetches', auth.redirectWhenLoggedOut('/login'), async (ctx) => {
 });
 
 router.get('/transfers', auth.redirectWhenLoggedOut('/login'), async (ctx) => {
+  let page;
+
+  if (!ctx.query.page) {
+    page = 1;
+  } else {
+    page = Number(ctx.query.page);
+  }
+
+  if (!Number.isInteger(page) || page < 1) {
+    ctx.body = 'Expected positive integer for page number!';
+    ctx.status = 400;
+    return;
+  }
+
+  const offset = (page - 1) * RESULTS_LIMIT;
   const dbClient = ctx.state.dbClient;
   const defaultContext = await getAdminContext(ctx, 'get', '/transfers');
-  const accountTransfersUsersRows =
-    await dbClient.selectAccountTransfersUsers();
+  const selectResult = await dbClient.executeQuery(`
+
+    SELECT
+      account_transfers.id AS account_transfer_id,
+      transfer_amount,
+      transferred_at,
+      account_transfers.user_id AS account_owner_id,
+      u1.email AS account_owner_email,
+      u2.id AS user_transferrer_id,
+      u2.email AS user_transferrer_email,
+      a1.name AS user_subscr_airport_from_name,
+      a2.name AS user_subscr_airport_to_name,
+      users_subscriptions.date_from AS user_subscr_date_from,
+      users_subscriptions.date_from AS user_subscr_date_to,
+      a3.name AS subscr_airport_from_name,
+      a4.name AS subscr_airport_to_name,
+      fetch_time
+    FROM account_transfers
+    LEFT JOIN users u1
+    ON account_transfers.user_id = u1.id
+    LEFT JOIN user_subscription_account_transfers
+    ON user_subscription_account_transfers.account_transfer_id = account_transfers.id
+    LEFT JOIN subscriptions_fetches_account_transfers
+    ON subscriptions_fetches_account_transfers.account_transfer_id = account_transfers.id
+    LEFT JOIN account_transfers_by_admin
+    ON account_transfers_by_admin.account_transfer_id = account_transfers.id
+    LEFT JOIN users u2
+    ON u2.id = account_transfers_by_admin.admin_user_id
+    LEFT JOIN users_subscriptions
+    ON user_subscription_account_transfers.user_subscription_id = users_subscriptions.id
+    LEFT JOIN subscriptions s1
+    ON users_subscriptions.subscription_id = s1.id
+    LEFT JOIN airports a1
+    ON s1.airport_from_id = a1.id
+    LEFT JOIN airports a2
+    ON s1.airport_to_id = a2.id
+    LEFT JOIN subscriptions_fetches
+    ON subscriptions_fetches_account_transfers.subscription_fetch_id = subscriptions_fetches.id
+    LEFT JOIN subscriptions s2
+    ON subscriptions_fetches.subscription_id = s2.id
+    LEFT JOIN airports a3
+    ON s2.airport_from_id = a3.id
+    LEFT JOIN airports a4
+    ON s2.airport_to_id = a4.id
+    LEFT JOIN fetches
+    ON subscriptions_fetches.fetch_id = fetches.id
+    ORDER BY account_transfer_id
+    LIMIT $1
+    OFFSET $2;
+
+  `, [RESULTS_LIMIT, offset]);
+
+  assertApp(isObject(selectResult), `Expected selectResult to be an object, but was ${typeof selectResult}`);
+  assertApp(Array.isArray(selectResult.rows), `Expected selectResult.rows to be array, but was ${typeof selectResult.rows}`);
+
+  const accountTransfersUsersRows = selectResult.rows;
+
   const accountTransfers = accountTransfersUsersRows.map(row => {
     const expectRequiredRowProps = [
       {
@@ -426,6 +496,9 @@ router.get('/transfers', auth.redirectWhenLoggedOut('/login'), async (ctx) => {
     total_credits_loaded: totalCreditsLoaded.rows[0].users_given_credits,
     dalipeche_api_total_requests:
       dalipecheAPITotalRequests.rows[0].dalipeche_api_requests,
+    page,
+    next_page: accountTransfers.length === RESULTS_LIMIT ? page + 1 : null,
+    prev_page: page > 1 ? page - 1 : null,
   });
 });
 
