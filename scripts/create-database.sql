8a6521c56634ec70328cfad1e0cdb493c1f8d7f6
Antonio Todorov <taeskow@gmail.com>
2018-08-22 16:05:02 +0300

    Add login_sessions table to database.
    Active login sessions will be added to this table with a uniquely
    generated random token through the generate_session_token() function.
    
    Expired login sessions must be deleted, from this table.

--- scripts/create-database.sql
+++ scripts/create-database.sql
@@ -162,6 +162,27 @@ ON users_subscriptions(created_at);
 CREATE INDEX users_subscriptions_updated_at_idx
 ON users_subscriptions(updated_at);
 
+CREATE FUNCTION generate_session_token() RETURNS text AS $$
+DECLARE
+    new_token text;
+    done bool;
+BEGIN
+    done := false;
+    WHILE NOT done LOOP
+        new_token := md5(random()::text);
+        done := NOT EXISTS(SELECT 1 FROM login_sessions WHERE token=new_token);
+    END LOOP;
+    RETURN new_token;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+CREATE TABLE login_sessions (
+    id serial PRIMARY KEY,
+    user_id integer UNIQUE NOT NULL REFERENCES users ON DELETE CASCADE,
+    expiration_date timestamp NOT NULL DEFAULT current_timestamp + interval '1 day',
+    token text UNIQUE NOT NULL CHECK (char_length(token) > 25) DEFAULT generate_session_token()
+)
+
 CREATE TABLE routes (
   id serial PRIMARY KEY NOT NULL,
   booking_token text NOT NULL UNIQUE,
