7f45ac6560297aa30b8d8484cde9bc3028672c21
Hristo Spasov <hristo_sp99@abv.bg>
2018-08-29 10:23:27 +0300

    Remove edit user option from all users page

--- admin/static/js/users.js
+++ admin/static/js/users.js
@@ -20,7 +20,6 @@ function start () {
     });
 
     $usersTable.find('tbody tr')
-      .not('#user-edit-mode')
       .not('#user-view-mode')
       .remove();
   }
@@ -93,7 +92,7 @@ function start () {
 
     const modes = {
       'view': renderUserRowViewMode,
-      'edit': renderUserRowEditMode,
+      // edit mode not needed
     };
 
     assertApp(typeof modes[mode] === 'function', {
@@ -116,16 +115,15 @@ function start () {
 
     $userViewModeClone.find('#user-view-mode-email')
       .attr('id', 'user-view-mode-email-' + rowId) // eslint-disable-line prefer-template
-      .attr('href', '/users/' + user.id) // eslint-disable-line prefer-template
       .text(user.email);
 
     $userViewModeClone.find('#user-view-mode-credits')
       .attr('id', 'user-view-mode-credits-' + rowId) // eslint-disable-line prefer-template
       .text(user.credits);
 
-    $userViewModeClone.find('#user-view-mode-edit-btn')
-      .attr('id', 'user-view-mode-edit-btn-' + rowId) // eslint-disable-line prefer-template
-      .click(onEditUserClick);
+    $userViewModeClone.find('#user-view-mode-open-btn')
+      .attr('id', 'user-view-mode-open-btn-' + rowId) // eslint-disable-line prefer-template
+      .click(onOpenUserClick(user.id));
 
     if ($row == null) {
       $userViewModeClone.appendTo(
@@ -134,43 +132,6 @@ function start () {
     } else {
       $row.replaceWith($userViewModeClone);
     }
-  }
-
-  function renderUserRowEditMode (user, rowId, $row) {
-    mainUtils.trace('renderUserRowEditMode');
-
-    const $userEditModeClone = $('#user-edit-mode').clone()
-      .removeAttr('hidden')
-      .attr('id', 'user-' + rowId); // eslint-disable-line prefer-template
-
-    $userEditModeClone.find('#user-edit-mode-id')
-      .attr('id', 'user-edit-mode-id-' + rowId) // eslint-disable-line prefer-template
-      .text(user.id);
-
-    $userEditModeClone.find('#user-edit-mode-email')
-      .attr('id', 'user-edit-mode-email-' + rowId) // eslint-disable-line prefer-template
-      .attr('value', user.email);
-
-    $userEditModeClone.find('#user-edit-mode-password')
-      .attr('id', 'user-edit-mode-password-' + rowId); // eslint-disable-line prefer-template
-
-    $userEditModeClone.find('#user-edit-mode-credits')
-      .attr('id', 'user-edit-mode-credits-' + rowId) // eslint-disable-line prefer-template
-      .text(user.credits);
-
-    $userEditModeClone.find('#user-edit-mode-save-btn')
-      .attr('id', 'user-edit-mode-save-btn-' + rowId) // eslint-disable-line prefer-template
-      .click(onSaveUserClick);
-
-    $userEditModeClone.find('#user-edit-mode-cancel-btn')
-      .attr('id', 'user-edit-mode-cancel-btn-' + rowId) // eslint-disable-line prefer-template
-      .click(onCancelEditUserClick);
-
-    $userEditModeClone.find('#user-edit-mode-remove-btn')
-      .attr('id', 'user-edit-mode-remove-btn-' + rowId) // eslint-disable-line prefer-template
-      .click(onRemoveUserClick);
-
-    $row.replaceWith($userEditModeClone);
   }
 
   function showUsersResults () {
@@ -183,166 +144,13 @@ function start () {
     $('#users-results').attr('hidden', true);
   }
 
-  const onRemoveUserClick = function (event) {
-    mainUtils.trace('clicked on remove user button');
-
-    const removeButton = event.target;
-    const rowId = mainUtils.getElementUniqueId(removeButton, 'user-edit-mode-remove-btn-');
-    const oldUser = rowIdUserMap[rowId];
-
-    removeButton.disabled = true;
-
-    const removeUserParams = {
-      v: '2.0',
-      user_id: oldUser.id,
-      api_key: APIKeyRef.APIKey,
+  const onOpenUserClick = function (userId) {
+    return function (event) {
+      window.location = '/users/' + userId; // eslint-disable-line prefer-template
     };
+  }
 
-    adminAPI.adminRemoveUser(
-      removeUserParams,
-      PROTOCOL_NAME,
-      function (result) { // eslint-disable-line prefer-arrow-callback
-        removeButton.disabled = false;
-
-        if (result.status_code === '1000') {
-          users = users.filter(function (user) { // eslint-disable-line prefer-arrow-callback
-            return user.id !== oldUser.id;
-          });
-
-          delete rowIdUserMap[rowId];
-
-          $('#user-' + rowId).remove(); // eslint-disable-line prefer-template
-
-          if (users.length > 0) {
-            showUsersResults();
-          } else {
-            hideUsersResults();
-          }
-          mainUtils.displayUserMessage('Successfully removed user!', 'success');
-        } else {
-          mainUtils.displayUserMessage('Remove user failed with status code: ' + result.status_code, 'error'); // eslint-disable-line prefer-template
-        }
-      }
-    );
-  };
-
-  const onEditUserClick = function (event) {
-    mainUtils.trace('clicked on edit user button');
-
-    const rowId = mainUtils.getElementUniqueId(event.target, 'user-view-mode-edit-btn-');
-    const user = rowIdUserMap[rowId];
-
-    renderUserRow(
-      'edit',
-      user,
-      $('#user-' + rowId) // eslint-disable-line prefer-template
-    );
-  };
-
-  const onCancelEditUserClick = function (event) {
-    mainUtils.trace('clicked on cancel edit user button');
-
-    const rowId = mainUtils.getElementUniqueId(event.target, 'user-edit-mode-cancel-btn-');
-    const user = rowIdUserMap[rowId];
-
-    renderUserRow(
-      'view',
-      user,
-      $('#user-' + rowId) // eslint-disable-line prefer-template
-    );
-  };
-
-  const onSaveUserClick = function (event) {
-    mainUtils.trace('clicked on save user button');
-
-    const saveButton = event.target;
-
-    const rowId = mainUtils.getElementUniqueId(event.target, 'user-edit-mode-save-btn-');
-    const user = rowIdUserMap[rowId];
-
-    const newEmail = $('#user-edit-mode-email-' + rowId).val().trim(); // eslint-disable-line prefer-template
-    const newPassword = $('#user-edit-mode-password-' + rowId).val().trim(); // eslint-disable-line prefer-template
-
-    const params = {
-      v: '2.0',
-      api_key: APIKeyRef.APIKey,
-      user_id: user.id,
-    };
-
-    if (newEmail.length > 0) {
-      params.email = newEmail;
-    }
-
-    if (newPassword.length > 0) {
-      params.password = newPassword;
-    }
-
-    saveButton.disabled = true;
-
-    // function lock(func) {
-    //   var flag = false;
-    //   function wrapped(...args) {
-    //     if (flag) {
-    //       throw;
-    //     }
-    //     flag = true;
-    //     try {
-    //       result = func(...args)
-    //     } finally {
-    //       flag = false;
-    //     }
-    //     return result;
-    //   }
-    //   return wrapped;
-    // }
-
-    // adminEditUser(params, asdfasdf, lock(function(result) {
-    //   console.log(result);
-    // }))
-
-    adminAPI.adminEditUser(params, PROTOCOL_NAME, function (result) { // eslint-disable-line prefer-arrow-callback
-      saveButton.disabled = false;
-
-      const messagesByCode = {
-        '1000': 'Successfully updated user!',
-        '2001': 'Email is taken.',
-        '2000': 'An error occurred and could not edit user credentials.',
-        '2100': 'You have an invalid api key.',
-        '2201': 'Email is too short.',
-        '2202': 'Password is too short.',
-        '2203': 'Email is not valid.',
-        '2204': 'Email is taken',
-      };
-      const defaultErrorMessage = 'Edit user failed with status code: ' + result.status_code; // eslint-disable-line prefer-template
-
-      var message; // eslint-disable-line no-var
-
-      if (messagesByCode[result.status_code] == null) {
-        message = defaultErrorMessage;
-      } else {
-        message = messagesByCode[result.status_code];
-      }
-
-      if (result.status_code === '1000') {
-        const newUser = {
-          id: user.id,
-          credits: user.credits,
-          email: (newEmail.length > 0) ? newEmail : user.email,
-        };
-
-        renderUserRow(
-          'view',
-          newUser,
-          $('#user-' + rowId) // eslint-disable-line prefer-template
-        );
-        mainUtils.displayUserMessage(message, 'success');
-      } else {
-        mainUtils.displayUserMessage(message, 'error');
-      }
-    });
-  };
-
-  const onPreviousPageClick = function (event) { // eslint-disable-line prefer-arrow-callback
+  const onPreviousPageClick = function (event) {
     mainUtils.trace('onPreviousPageClick');
 
     const button = event.target;
